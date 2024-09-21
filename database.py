3b1404ae2a402747b9de5d91ecc43852bb5b3896
Oleh Smolynets <smolynets@gmail.com>
2024-09-21 11:42:39 +0300

    add postgres

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://fastapi_user:fastapi_password@localhost:5433/fastapi_db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
